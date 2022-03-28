81d4af2c1d57dcc3ccbe5fc9f4ceffc27025a239
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-03-28 20:23:18 -0300

    Atualização do projeto

--- UniClima/src/java/br/uninove/uniclima/http/Http.java
+++ UniClima/src/java/br/uninove/uniclima/http/Http.java
@@ -1,5 +1,13 @@
 package br.uninove.uniclima.http;
 
+import br.uninove.clima.Clima;
+
 public class Http {
+    
+    public static Clima getClima(String cidade) {
+        String url = "";
+        
+        return null;
+    }
     
 }
