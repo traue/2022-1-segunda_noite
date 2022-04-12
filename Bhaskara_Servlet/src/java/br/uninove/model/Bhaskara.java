714db6609be0b2baac06d4f9f205999ba04e8e41
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-04-11 21:58:49 -0300

    Criação e desenvolvimento inicial do projeto

---
+++ Bhaskara_Servlet/src/java/br/uninove/model/Bhaskara.java
@@ -0,0 +1,13 @@
+package br.uninove.model;
+
+public class Bhaskara {
+    int a, b, c;
+    double delta, xP, xN;
+    
+    //paramos aqui...
+    //o que falta: 
+    // - encapsular os campos
+    // - criar os méodos de cálculo (delta, xP e xN)
+    // - Criar o controller..
+    // - ...
+}
